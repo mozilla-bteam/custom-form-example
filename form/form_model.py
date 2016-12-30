d2043d9446a015e18ad6ea038084204deeb2c3a2
Sebastin Santy <sebastinssanty@gmail.com>
2016-12-30 16:57:51 +0530

    Metaclass for dynamic forms

--- form/form_model.py
+++ form/form_model.py
@@ -2,16 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, DateField, RadioField, TextAreaField, SelectField, SelectMultipleField
 from wtforms import validators
 
-class CustomForm(FlaskForm):
-	name = StringField('Name', [validators.DataRequired(), validators.Length(min=4, max=25)])
-	email = StringField('Email Address', [validators.Length(min=6, max=35)])
-	dob = DateField('Date of Birth', format='%m/%d/%Y')
-	lang = RadioField('Your Primary Language', choices=[('english','English'),('french','French'),('spanish','Spanish'),('portugese','Portugese'),('hindi','Hindi')])
-	tier = SelectField('Your Tier?', choices=[(1, "One"), (2, "Two"), (2, "Three")], default=1)
-	gears = SelectMultipleField('Gears you want?', choices=[('shirt', "Firefox Shirt"), ('cap', "Firefox Cap"), ('hoodie', "Mozilla Hoodie")],)
-	comments = TextAreaField('Any Comments?', [validators.DataRequired()])
-	terms = BooleanField('I accept terms', [validators.DataRequired()])
-
-# class LoginForm(FlaskForm):
-# 	username = StringField('Username', [validators.DataRequired()])
-# 	password = PasswordField('Password', [validators.DataRequired()])
+CustomForm = type('CustomForm', (FlaskForm,) ,{
+		'name': StringField('Name', [validators.DataRequired(), validators.Length(min=4, max=25)]),
+		'email': StringField('Email Address', [validators.Length(min=6, max=35)]),
+		'dob': DateField('Date of Birth', format='%m/%d/%Y'),
+		'lang': RadioField('Your Primary Language', choices=[('english','English'),('french','French'),('spanish','Spanish'),('portugese','Portugese'),('hindi','Hindi')]),
+		'tier': SelectField('Your Tier?', choices=[(1, "One"), (2, "Two"), (2, "Three")], default=1),
+		'gears': SelectMultipleField('Gears you want?', choices=[('shirt', "Firefox Shirt"), ('cap', "Firefox Cap"), ('hoodie', "Mozilla Hoodie")],),
+		'comments': TextAreaField('Any Comments?', [validators.DataRequired()]),
+		'terms': BooleanField('I accept terms', [validators.DataRequired()])
+	})
